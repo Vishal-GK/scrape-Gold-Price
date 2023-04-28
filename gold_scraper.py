69a341ba6eac2185009368ddd22e8ea59670088a
Vishal-GK <129185339+Vishal-GK@users.noreply.github.com>
2023-04-28 15:23:25 +0530

    Update gold_scraper.py
    add usa price data

--- gold_scraper.py
+++ gold_scraper.py
@@ -1,15 +1,16 @@
 from bs4 import BeautifulSoup
-import requests,math
+import requests
 from datetime import date
 import pandas as pd
+from lxml import html
 
+session = requests.Session()
 today = date.today()
 
 def indian_gold_data():
-    data = requests.get("https://www.kitco.com/gold-price-today-india/")
-    soup = BeautifulSoup(data.text, 'html.parser')
+    response = session.get("https://www.kitco.com/gold-price-today-india/")
+    soup = BeautifulSoup(response.text, 'lxml')
 
-    date = soup.find('div',class_='table-price--body-table').p.text.strip()
     price_table = soup.find('div',class_='table-price--body-table').find('div',class_='table-price--body-table--overview-detail').table
     # for i in price_table.find_all('td'):
         # print(i.text.strip())
@@ -27,7 +28,7 @@ def indian_gold_data():
     df = pd.DataFrame(rows, columns=col)
     Without_gst_price = df['Gold Price Today'][1].replace(',','')
     print(f'Without gst prise: {Without_gst_price}')
-    With_gst_price = math.ceil((1.18 * float(Without_gst_price))/1.03)
+    With_gst_price = round((1.18 * float(Without_gst_price))/1.03)
     print(f'With gst price: {With_gst_price}')
 
 
@@ -43,14 +44,40 @@ def tamilnadu_gold_data():
 
     df = pd.DataFrame(rows, columns=['Gram','24K Today','24K Yesterday','Price Change'])
     Without_gst_price = float(df['24K Today'][0].lstrip('₹').replace(',','').strip())
-    print(f'Without gst prise: {Without_gst_price}')
-    With_gst_price = math.ceil(Without_gst_price/1.03)
-    print(f'With gst price: {With_gst_price}')
+    print(f'With gst prise: {Without_gst_price}')
+    With_gst_price = round(Without_gst_price/1.03)
+    print(f'Final gst price: {With_gst_price}')
 
+
+def usa_gold_price():
+    response = session.get('https://www.kitco.com/gold-price-today-usa/')
+    tree = html.fromstring(response.text,'lxml')
+    rows = []
+
+    for i in tree.xpath('//div[@class="table-price--body-table--overview-detail"]/table/tr')[1:3]:
+        rows.append(i.xpath('td/text()')[:2])
+
+    print(f'24 KT gold price: {rows[1][1]}')
+
+    kt_22 = round((float(rows[1][1])*22)/24,2)
+    print(f'24 KT gold price: {kt_22}')
+    rows.append(['22 KT Gold price per gram',kt_22])
+
+    kt_18 = round((float(rows[1][1])*18)/24,2)
+    print(f'18 KT gold price: {kt_18}')
+    rows.append(['18 KT Gold price per gram',kt_18])
+
+    kt_14 = round((float(rows[1][1])*14)/24,2)
+    print(f'14 KT gold price: {kt_14}')
+    rows.append(['14 KT Gold price per gram',kt_14])
+
+    kt_10 = round((float(rows[1][1])*10)/24,2)
+    print(f'10 KT gold price: {kt_10}')
+    rows.append(['10 KT Gold price per gram',kt_10])
 
 
 get_input = input("Enter Region: ")
-if get_input == 'Tamilnadu':
-    tamilnadu_gold_data()
-else:
-    indian_gold_data()
+
+if get_input == 'Tamilnadu':tamilnadu_gold_data()
+elif get_input == 'USA':usa_gold_price()
+else:indian_gold_data()
